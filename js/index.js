239be01dbf6e3428eec3aafdf6615be3a318af8d
divyanshu <vadhldivyesh@gmail.com>
2025-08-01 16:20:00 +0530

    pwa intregration

--- js/index.js
+++ js/index.js
@@ -624,6 +624,19 @@ function initializeEventListeners() {
   }
 }
 
+// Service Worker Registration
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
+
 // Initialize the app
 loadFromLocalStorage();
 renderAllTasksSorted();
